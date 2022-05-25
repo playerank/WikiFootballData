b9f948add2f55175aa3bdc6142f3d0277764d247
DodoAngels <104068005+DodoAngels@users.noreply.github.com>
2022-05-25 10:19:17 +0200

    Update mainAPI.py

--- back_end/API/mainAPI.py
+++ back_end/API/mainAPI.py
@@ -1,15 +1,15 @@
-from fastapi import FastAPI, responses, Depends
+# from typing import Set
+from fastapi import FastAPI, responses
 #from .dependencies import get_query_token
 import usersAPI
 import matchesAPI
+# from lib.user import User
 
 app = FastAPI()#dependencies=[Depends(get_query_token)])
 app.include_router(usersAPI.router)
 app.include_router(matchesAPI.router)
 
 n=5
-
-tmp_user_database=[{"username":"Dodo","password":"12"}]
 
 @app.get("/")
 async def root():
@@ -41,8 +41,15 @@ async def change_N(new_value: int):
     """
     Change the value of N, it's not retroactive, if new_value is incorrect return error
     """
-    if new_value>len(tmp_user_database):
+    if new_value>len(usersAPI.tmp_user_database):
         return responses.JSONResponse(content={"message":"value is incorrect"},status_code=400)
     global n
     n=abs(new_value)
     return {"message":"N updated successfully!"}
+
+@app.get("/rules")
+async def get_N():
+    """
+    Return the value of N, only administrators can call this funcion
+    """
+    return n
