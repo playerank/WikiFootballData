8e73c45d40367b69beab1a1d22a64f6859bb2d5e
DodoAngels <104068005+DodoAngels@users.noreply.github.com>
2022-07-02 22:59:47 +0200

    Add files via upload

--- back_end/API/data/matches.py
+++ back_end/API/data/matches.py
@@ -5,11 +5,10 @@ class Match(mongoengine.Document):
     Class defining a match.
     """
     competition_id=mongoengine.ObjectIdField(required=True)
-    round_id=mongoengine.IntField(required=True)
-    season_id=mongoengine.IntField(required=True)
-    date_utc=mongoengine.DateTimeField(required=True)#o int
-    match_id=mongoengine.IntField(required=True)
-    label=mongoengine.StringField(required=True)
+    round=mongoengine.StringField(required=True)
+    season=mongoengine.StringField(required=True)
+    date_utc=mongoengine.DateTimeField(required=True)
+    #label=mongoengine.StringField(required=True)
     home_team_id=mongoengine.ObjectIdField(required=True)
     away_team_id=mongoengine.ObjectIdField(required=True)
     additional_attributes=mongoengine.ListField()
@@ -18,7 +17,7 @@ class Match(mongoengine.Document):
     username=mongoengine.StringField(required=True)
     report=mongoengine.FileField() #??
     report_is_confirmed=mongoengine.BooleanField(default=False)
-    journal=mongoengine.ListField()
+    journal=mongoengine.ListField(default=["Match created!"])
     data=mongoengine.ListField()
     working=mongoengine.ListField()
     is_confirmed=mongoengine.BooleanField(default=False)
