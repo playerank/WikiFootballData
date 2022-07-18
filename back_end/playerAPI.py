af89ed2db9afe53490761ac2513e90a8e3602e69
DodoAngels <104068005+DodoAngels@users.noreply.github.com>
2022-07-18 12:01:14 +0200

    Delete playerAPI.py

--- back_end/playerAPI.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, responses#, Depends
-#from .dependencies import get_current_username
-
-router = APIRouter(
-    prefix="/players",
-    tags=["players"]#,#Possibile implementazione della sicurezza
-    # dependencies=[Depends(get_current_username)],
-    # responses={404: {"description":"not found"}},
-)
