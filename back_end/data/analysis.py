ea64128d6c00982aa7448b4c8a18b0f9be3e84d1
DodoAngels <104068005+DodoAngels@users.noreply.github.com>
2022-07-18 12:29:20 +0200

    Add files via upload

--- back_end/data/analysis.py
+++ back_end/data/analysis.py
@@ -11,24 +11,12 @@ class Analysis(mongoengine.EmbeddedDocument):
     endorsements: int=mongoengine.IntField(required=True)
     dislikes: int=mongoengine.IntField(required=True)
 
-    # @property
-    # def init(self,time_slot: str):
-    #     """
-    #     Initializes an empy object analysis
-    #     """
-    #     self.time_slot=time_slot
-    #     self.detail=None
-    #     self.working=""
-    #     self.author=""
-    #     self.endorsements=0
-    #     self.dislikes=0
-
 def init(time_slot: str) -> Analysis:
     a=Analysis()
     a.time_slot=time_slot
     a.detail=None
-    a.working=""
-    a.author=""
+    a.working=None
+    a.author=None
     a.endorsements=0
     a.dislikes=0
     return a
