80b78a0448b9299fe926c787e94f016ded34c194
DodoAngels <104068005+DodoAngels@users.noreply.github.com>
2022-07-31 13:46:37 +0200

    Add files via upload

---
+++ back_end/data/players.py
@@ -0,0 +1,17 @@
+import mongoengine
+
+class Player(mongoengine.Document):
+    """
+    Class defining a player
+    """
+    player_name: str=mongoengine.StringField(required=True)
+    nationality: str=mongoengine.StringField(required=True)
+    current_team: str=mongoengine.StringField(required=True)
+    club_shirt_number: int=mongoengine.IntField(required=True)
+    national_team_shirt_number: int=mongoengine.IntField(default=-1)
+    is_confirmed: bool=mongoengine.BooleanField(default=False)
+
+    meta={
+        'db_alias': 'core',
+        'collection': 'players'
+    }
