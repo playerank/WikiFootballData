061b7c44f3f342b852275dabe48d55d2cb93cbdf
DodoAngels <104068005+DodoAngels@users.noreply.github.com>
2022-08-10 12:49:17 +0200

    Add files via upload

--- back_end/data/players.py
+++ back_end/data/players.py
@@ -1,13 +1,15 @@
 import mongoengine
 from bson import ObjectId
+from datetime import datetime
 
 class Player(mongoengine.Document):
     """
     Class defining a player
     """
     player_name: str=mongoengine.StringField(required=True)
-    nationality: str=mongoengine.StringField(required=True)
-    current_team: ObjectId=mongoengine.ObjectIdField(required=True)
+    date_of_birth: datetime=mongoengine.DateTimeField(required=True)
+    team_id: ObjectId=mongoengine.ObjectIdField(required=True)
+    national_team_id: ObjectId=mongoengine.ObjectIdField(required=True)
     club_shirt_number: int=mongoengine.IntField(required=True)
     national_team_shirt_number: int=mongoengine.IntField(default=-1)
     is_confirmed: bool=mongoengine.BooleanField(default=False)
